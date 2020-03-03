dbc8605a8479457afcf1f88b50d2e6c34ab5d887
Aurora de Tagyos <aurora.detagyos@gmail.com>
2020-03-03 14:51:50 -0800

    skeleton code for starting game

---
+++ fungame.py
@@ -0,0 +1,20 @@
+# Aurora de Tagyos for web-app-intro for Phill Conrad, W20, CCS CS20
+
+import os
+from flask import Flask, url_for, render_template, request, session
+app = Flask(__name__)
+app.secret_key='w98fw9ef8hwe98fhwef'     #this sets the secret key for sessions
+
+@app.route("/")
+def hello():
+    return render_template('home.html')
+
+@app.route('/newgame')
+def render_newgame():
+    session['name']=request.args['name']
+    return render_template('newgame.html')
+
+
+
+if __name__=="__main__":
+    app.run(debug=False, port=54321)
